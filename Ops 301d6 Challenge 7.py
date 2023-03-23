26d45a8b30fdd17f85edb03ad922a0753dfa3cb9
JesusCEVC <123131865+JesusCEVC@users.noreply.github.com>
2023-03-22 17:04:50 -0700

    Create Ops 301d6 Challenge 7.py

---
+++ Ops 301d6 Challenge 7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# A list to get my head in the game
+characters = ["Mario", "Donkey Kong", "Link", "Samus", "Yoshi", "Kirby", "Fox", "Pikachu", "Luigi", "Captain Falcon"]
+
+# Show me some opponents
+print("Fourth element:", characters[3])
+
+# Show me some more opponents
+print("Sixth through tenth elements:", characters[5:10])
+
+# Im gonna make this particular player cry when I win, hence:
+characters[6] = "Onion"
+
+# Here is my list of remaining opponents
+print("Updated list:", characters)
