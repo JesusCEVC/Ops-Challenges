28e73a407269fa1f3ee6c80a094af8311ca61377
JesusCEVC <123131865+JesusCEVC@users.noreply.github.com>
2023-03-23 19:54:32 -0700

    Create Ops 301d6 Challenge.py

---
+++ Ops 301d6 Challenge.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Unequals
+luffy_crew_members = 10
+blackbeard_crew_members = 13
+
+if luffy_crew_members != blackbeard_crew_members:
+    print("Luffy's crew and Blackbeard's crew are different sizes.")
+
+# Greater or equal
+zoro_bounty = 320000000
+sanji_bounty = 330000000
+
+if zoro_bounty >= sanji_bounty:
+    print("Zoro's bounty is greater than or equal to Sanji's bounty.")
+
+# Equal
+luffy_fruit = "Nika Nika no Mi"
+ace_fruit = "Mera Mera no Mi"
+
+if luffy_fruit == "Nika Nika no Mi":
+    print("Luffy's Devil Fruit is the Nika Nika no Mi.")
+elif luffy_fruit == "Mera Mera no Mi":
+    print("Luffy's Devil Fruit is the Mera Mera no Mi.")
+else:
+    print("Luffy's Devil Fruit is not known.")
+
+# Less or equal
+usopp_courage = 50
+nami_courage = 100
+
+if usopp_courage <= nami_courage:
+    print("Usopp is more scared or just as scared as nami.")
+
+# Less than
+chopper_bounty = 100
+
+if chopper_bounty < 500:
+    print("Chopper's bounty is less than 500 berries.")
+
+# Greater than
+zoro_bounty = 150000000
+luffy_bounty = 320000000
+
+if zoro_bounty > luffy_bounty:
+    print("Zoro has a higher bounty than Luffy.")
+
+# If elif statement
+luffy_power = "Yonko Level"
+
+if luffy_power == "Yonko Level":
+    print("Luffy's power is Yonko Level.")
+elif luffy_power == "Gear Supernova Level":
+    print("Luffy's power is Supernova Level.")
+else:
+    print("Luffy's power is not known.")
+
+# If elif and else statement
+usopp_power = "His Bravery"
+
+if usopp_power == "His Bravery":
+    print("Usopp's power is his bravery.")
+elif usopp_power == "Usopp Golden Pound":
+    print("Usopp's power is the Usopp Golden Pound.")
+else:
+    print("Usopp's power is not known.")
