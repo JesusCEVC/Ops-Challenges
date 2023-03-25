f4e250fa1dc064f063541ce81f0a2ca0498206ad
JesusCEVC <123131865+JesusCEVC@users.noreply.github.com>
2023-03-24 17:06:44 -0700

    Update Ops 301d6: Challenge 9.py

--- Ops 301d6: Challenge 9.py
+++ Ops 301d6: Challenge 9.py
@@ -2,12 +2,13 @@
 
 # Create the file, then add the lines.
 with open('Boku_no_Hero_Academia.txt', 'a') as Dynamight:
-    Dynamight.write("I enjoy BNHA.\n")
+    first_line = "I enjoy BNHA.\n"
+    Dynamight.write(first_line)
     Dynamight.write("The recent seasons make it worth the middle series slog.\n")
-    Dynamight.write("The creativity is palpable!\n")
+    Dynamight.write("The creativity is palpable!")
 
 # print the first line of the file to the screen
-with open('Boku_no_Hero_Academia.txt', 'r') as Dynamight:
+with open('Boku_no_Hero_Academia.txt') as Dynamight:
     print(Dynamight.readline())
 
 # delete the file
