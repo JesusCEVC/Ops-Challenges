4aa1c0a6de70bdc6660807c75c211a590ede1157
JesusCEVC <123131865+JesusCEVC@users.noreply.github.com>
2023-05-03 16:30:01 -0700

    Update Ops 401d6: Challenge Class 11.py

--- Ops 401d6: Challenge Class 11.py
+++ Ops 401d6: Challenge Class 11.py
@@ -23,44 +23,23 @@ def tcp_port_scan(target_ip, port_range):
             print(f"Port {port} is filtered and silently dropped.")
     return open_ports
 
-def icmp_ping_sweep(network_address):
-    # Create a list of all addresses in the given network
-    network = IPv4Network(network_address, strict=False)
-    hosts = list(network.hosts())
+def network_scanner(target_ip):
+    # Send ICMP echo request to check if host is responsive
+    response = sr1(IP(dst=target_ip)/ICMP(), timeout=1, verbose=0)
 
-    # Ping all addresses on the given network except for network address and broadcast address
-    online_hosts = []
-    for host in hosts:
-        if host != network.network_address and host != network.broadcast_address:
-            response = sr1(IP(dst=str(host))/ICMP(), timeout=1, verbose=0)
-            if response is None:
-                print(f"Host {host} is down/unresponsive.")
-            elif response.type == 3 and response.code in [1, 2, 3, 9, 10, 13]:
-                print(f"Host {host} is actively blocking ICMP traffic.")
-            else:
-                print(f"Host {host} is responding.")
-                online_hosts.append(host)
-    
-    # Count how many hosts are online and inform the user
-    num_online_hosts = len(online_hosts)
-    print(f"{num_online_hosts} host(s) are online.")
-    return online_hosts
+    if response is None:
+        print(f"Host {target_ip} is down/unresponsive.")
+    elif response.type == 3 and response.code in [1, 2, 3, 9, 10, 13]:
+        print(f"Host {target_ip} is actively blocking ICMP traffic.")
+    else:
+        print(f"Host {target_ip} is responding.")
+        port_range = range(1, 1001)
+        open_ports = tcp_port_scan(target_ip, port_range)
+        print(f"{len(open_ports)} port(s) are open.")
+        
 
-# Prompt user for choice between TCP Port Range Scanner mode and ICMP Ping Sweep mode
-mode = input("Enter '1' for TCP Port Range Scanner mode, Enter '2' for ICMP Ping Sweep mode: ")
+# Prompt user for IP address to target
+target_ip = input("Enter the IP address to target: ")
 
-if mode == "1":
-    # Defines host IP and port range for the scan
-    target_ip = input("Enter your target IP: ")
-    port_range = range(int(input("Enter a starting port number: ")), int(input("Enter an ending port number: "))+1)
-    tcp_port_scan(target_ip, port_range)
-
-elif mode == "2":
-    # Prompt user for network address including CIDR block
-    network_address = input("Enter a network address (CIDR block): ")
-
-    # Perform ICMP Ping Sweep tool
-    icmp_ping_sweep(network_address)
-
-else:
-    print("Invalid.")
+# Perform network scanning
+network_scanner(target_ip)
